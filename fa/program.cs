db9182670106331d3f6c6b91df3040a3357c5ccc
Mirock <kashingeotgii@gmail.com>
2026-04-01 15:53:31 +0300

    Update program.cs

--- fa/program.cs
+++ fa/program.cs
@@ -6,37 +6,114 @@ using System.Threading.Tasks;
 
 namespace fans
 {
-  public class State
+  public class State(string name, bool isAcceptState)
   {
-    public string Name;
-    public Dictionary<char, State> Transitions;
-    public bool IsAcceptState;
+    public string Name = name;
+    public Dictionary<char, State> Transitions = [];
+    public bool IsAcceptState = isAcceptState;
   }
 
-
-  public class FA1
-  {
-    public bool? Run(IEnumerable<char> s)
+    public class FA1
     {
-      return false;
+        public static State a = new State("a", false);
+        public State b = new State("b", false);
+        public State c = new State("c",false);
+        public State d = new State("d", false);
+        public State e = new State("e", true);
+        State InitialState = a;
+
+        public FA1()
+        {
+            a.Transitions['0'] = b;
+            a.Transitions['1'] = c;
+            b.Transitions['0'] = d;
+            b.Transitions['1'] = e;
+            c.Transitions['0'] = e;
+            c.Transitions['1'] = c;
+            d.Transitions['0'] = d;
+            d.Transitions['1'] = d;
+            e.Transitions['0'] = d;
+            e.Transitions['1'] = e;
+        }
+
+        public bool? Run(IEnumerable<char> s)
+        {
+            State current = InitialState;
+            foreach (var c in s) // цикл по всем символам 
+            {
+                current = current.Transitions[c]; // меняем состояние на то, в которое у нас переход
+                if (current == null)              // если его нет, возвращаем признак ошибки
+                    return null;
+                // иначе переходим к следующему
+            }
+            return current.IsAcceptState;         // результат true если в конце финальное состояние 
+        }
     }
-  }
 
   public class FA2
   {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
+        public static State a = new State("a", false);
+        public State b = new State("b", false);
+        public State c = new State("c", false);
+        public State d = new State("d", true);
+        State InitialState = a;
+
+        public FA2()
+        {
+            a.Transitions['0'] = b;
+            a.Transitions['1'] = c;
+            b.Transitions['0'] = a;
+            b.Transitions['1'] = d;
+            c.Transitions['0'] = d;
+            c.Transitions['1'] = a;
+            d.Transitions['0'] = c;
+            d.Transitions['1'] = b;
+        }
+
+        public bool? Run(IEnumerable<char> s)
+        {
+            State current = InitialState;
+            foreach (var c in s) // цикл по всем символам 
+            {
+                current = current.Transitions[c]; // меняем состояние на то, в которое у нас переход
+                if (current == null)              // если его нет, возвращаем признак ошибки
+                    return null;
+                // иначе переходим к следующему
+            }
+            return current.IsAcceptState;         // результат true если в конце финальное состояние 
+        }
     }
-  }
   
   public class FA3
   {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
+        public static State a = new State("a", false);
+        public State b = new State("b", false);
+        public State c = new State("c", true);
+        State InitialState = a;
+
+        public FA3()
+        {
+            a.Transitions['0'] = a;
+            a.Transitions['1'] = b;
+            b.Transitions['0'] = a;
+            b.Transitions['1'] = c;
+            c.Transitions['0'] = c;
+            c.Transitions['1'] = c;
+        }
+
+        public bool? Run(IEnumerable<char> s)
+        {
+            State current = InitialState;
+            foreach (var c in s) // цикл по всем символам 
+            {
+                current = current.Transitions[c]; // меняем состояние на то, в которое у нас переход
+                if (current == null)              // если его нет, возвращаем признак ошибки
+                    return null;
+                // иначе переходим к следующему
+            }
+            return current.IsAcceptState;         // результат true если в конце финальное состояние 
+        }
     }
-  }
 
   class Program
   {
